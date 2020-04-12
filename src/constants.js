8ba05cfe95de48a01d3253996df379f857c757f2
jpogah <jpogah@gmail.com>
2020-04-12 01:11:12 -0500

    remove package json lock

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 
 
-export const API_BASE_URL = process.env.NODE_ENV !=='development' ? 'http://35.194.19.68:30367/api' : 'http://localhost:8080/api';
+export const API_BASE_URL = process.env.NODE_ENV !=='development' ? 'https://fast-forest-98219.herokuapp.com/api' : 'http://localhost:8080/api';
 
 
 export const headers = new Headers({
