505482cc4c5b1a1056ed7ee8f9d33101cec7f866
jpogah <jpogah@gmail.com>
2020-04-12 11:40:19 -0500

    add yarn back

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,6 @@
 
 
-export const API_BASE_URL = process.env.NODE_ENV !=='development' ? 'https://fast-forest-98219.herokuapp.com/api' : 'http://localhost:8080/api';
-
+export const API_BASE_URL = 'https://fast-forest-98219.herokuapp.com/api'
 
 export const headers = new Headers({
     'Authorization' : sessionStorage.getItem('jwtToken'),
